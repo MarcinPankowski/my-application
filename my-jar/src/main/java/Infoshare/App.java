3af4dcd980ce96960d508b71b540816e55d6edc4
Marcin P <marcin.pankowski@gmail.com>
2016-09-17 19:52:41 +0200

    Initial commit

---
+++ my-jar/src/main/java/Infoshare/App.java
@@ -0,0 +1,13 @@
+package Infoshare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
